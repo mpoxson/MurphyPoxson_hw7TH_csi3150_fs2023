2d20caf6a46c90a75a8309351900603f6ecdd516
mpoxson <mpoxson@gmail.com>
2023-11-14 19:07:43 -0500

    React + start function

---
+++ assignment7/src/component/SignUp.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+const SignUp = () => {
+  const [getter, infoSetter] = React.useState({
+    fName: "",
+    userName: "",
+    email: "",
+    address: "",
+    phoneNumber: "",
+  });
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    infoSetter((prevInfo) => ({
+      ...prevInfo,
+      [name]: value,
+    }));
+    console.log(getter);
+  }
+
+  return (
+    <main>
+      <div className="form">
+        <input
+          type="text"
+          className="form-input"
+          placeholder="Full Name"
+          name="fName"
+          value={getter.fName}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          className="form-input"
+          placeholder="Username"
+          name="userName"
+          value={getter.userName}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          className="form-input"
+          placeholder="Email"
+          name="email"
+          value={getter.email}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          className="form-input"
+          placeholder="address"
+          name="address"
+          value={getter.address}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          className="form-input"
+          placeholder="Phone Number"
+          name="phoneNumber"
+          value={getter.phoneNumber}
+          onChange={handleChange}
+        />
+      </div>
+    </main>
+  );
+};
+
+export default SignUp;
